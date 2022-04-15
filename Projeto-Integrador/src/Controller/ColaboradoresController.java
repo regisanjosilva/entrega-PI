cd02c030568857536c8d88642a211da3e6d15619
Regis <regisanjos@hotmail.com>
2022-04-15 06:05:45 -0300

    atualisando

--- Projeto-Integrador/src/Controller/ColaboradoresController.java
+++ Projeto-Integrador/src/Controller/ColaboradoresController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 
 
+
 import Dao.ColaboradoresDao;
 import Modelo.Colaboradores;
 
--- Projeto-Integrador/src/util/ConectionUtil.java
+++ Projeto-Integrador/src/util/ConectionUtil.java
@@ -1,6 +1,8 @@
 package util;
 
 import java.sql.Connection;
+
+
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
@@ -15,7 +17,7 @@ public class ConnectionUtil {
     public static Connection getConnection(){
         try { 
             Class.forName("com.mysql.cj.jdbc.Driver");
-            String url = "jdbc:mysql://localhost:3312/poo";
+            String url = "jdbc:mysql://127.0.0.1:3312/Projeto_Integrador";
             con = DriverManager.getConnection(url,"root","");
             con.setAutoCommit(true);
             return con;
